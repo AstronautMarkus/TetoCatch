e922d715ba4a4cd48755c896ef80b018afe3146d
astronautmarkus <astronautmarkus@gmail.com>
2024-12-31 18:10:07 -0300

    feat: Added initial React files

---
+++ TetoCatch/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TetoCatch',
+  webDir: 'dist'
+};
+
+export default config;
